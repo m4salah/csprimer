62baa6da83230c66293042949bf2e7e00ed5e433
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-07-20 20:01:07 +0300

    solve utf8 truncate

--- computer_systems/utf8_truncate/src/main.rs
+++ computer_systems/utf8_truncate/src/main.rs
@@ -4,28 +4,29 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-fn utf8_truncate() -> Result<(), Box<dyn Error>> {
+fn utf8_truncate(n: u8, buf: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
+    let mut n = n as usize;
+    if n >= buf.len() {
+        return Ok(Vec::from(buf));
+    }
+    while n > 0 && buf[n] & 0b11000000 == 0b10000000 {
+        n -= 1;
+    }
+    Ok(Vec::from(&buf[..n]))
+}
+fn main() -> Result<(), Box<dyn Error>> {
     let txt = File::open("utf8-truncate/cases")?;
     let mut txt = BufReader::new(txt);
     let mut buffer = Vec::new();
-
     while txt.read_until(b'\n', &mut buffer)? > 0 {
-        // Remove the newline character if you don't want it in the output
-        if let Some(&last) = buffer.last() {
-            if last == b'\n' {
-                buffer.pop();
-            }
-        }
+        let n = buffer[0];
+        // remove the new line b'\n'
+        buffer.pop();
 
-        // Print the buffer or process it as needed
-        println!("{:?}", buffer);
-
+        let res = utf8_truncate(n, &buffer[1..])?;
+        println!("{}", String::from_utf8_lossy(&res));
         // Clear the buffer for the next line
         buffer.clear();
     }
-
     Ok(())
-}
-fn main() -> Result<(), Box<dyn Error>> {
-    utf8_truncate()
 }
