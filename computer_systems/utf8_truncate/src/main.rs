551a25cbe43a27ab7de6ce85c2ebb0b466f78c61
Mohamed A. Salah <mohamed.a.salah11@gmail.com>
2024-07-24 02:06:42 +0300

    solve it by my way

--- computer_systems/utf8_truncate/src/main.rs
+++ computer_systems/utf8_truncate/src/main.rs
@@ -5,15 +5,57 @@ use std::{
 };
 
 fn utf8_truncate(n: u8, buf: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
-    let mut n = n as usize;
-    if n >= buf.len() {
-        return Ok(Vec::from(buf));
+    let n = n as usize;
+    let mut res = Vec::new();
+    let mut i = 0;
+    loop {
+        if i + 1 > n {
+            break;
+        }
+        if i >= buf.len() {
+            break;
+        }
+        if buf[i] & 0b10000000 == 0 {
+            // This is one byte character
+            res.push(buf[i]);
+            i += 1;
+        } else if buf[i] & 0b11110000 == 0b11110000 {
+            // four bytes utf-8 character
+            if i + 4 > n {
+                break;
+            }
+            res.push(buf[i]);
+            res.push(buf[i + 1]);
+            res.push(buf[i + 2]);
+            res.push(buf[i + 3]);
+            i += 4;
+        } else if buf[i] & 0b11100000 == 0b11100000 {
+            // three bytes utf-8 character
+            if i + 3 > n {
+                break;
+            }
+            res.push(buf[i]);
+            res.push(buf[i + 1]);
+            res.push(buf[i + 2]);
+            i += 3;
+        } else if buf[i] & 0b11000000 == 0b11000000 {
+            // four bytes utf-8 character
+            if i + 2 > n {
+                break;
+            }
+            res.push(buf[i]);
+            res.push(buf[i + 1]);
+            i += 2;
+        } else {
+            panic!(
+                "Unexpected utf-8 buf: hex: {:2X?}, bin:{:#10b} i:{i}",
+                buf[i], buf[i]
+            );
+        }
     }
-    while n > 0 && buf[n] & 0b11000000 == 0b10000000 {
-        n -= 1;
-    }
-    Ok(Vec::from(&buf[..n]))
+    Ok(res)
 }
+
 fn main() -> Result<(), Box<dyn Error>> {
     let txt = File::open("utf8-truncate/cases")?;
     let mut txt = BufReader::new(txt);
@@ -29,4 +71,40 @@ fn main() -> Result<(), Box<dyn Error>> {
         buffer.clear();
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn truncate_full_emoji_at_end() {
+        assert_eq!(
+            utf8_truncate(28, "Truncate full emoji at end ð¿".as_bytes()).unwrap(),
+            Vec::from("Truncate full emoji at end ".as_bytes())
+        )
+    }
+
+    #[test]
+    fn truncate_retain_full_emoji_at_end() {
+        assert_eq!(
+            utf8_truncate(29, "Retain full emoji at end 😍".as_bytes()).unwrap(),
+            Vec::from("Retain full emoji at end 😍".as_bytes())
+        )
+    }
+
+    #[test]
+    fn do_truncate() {
+        assert_eq!(
+            utf8_truncate(8, "Do truncate".as_bytes()).unwrap(),
+            Vec::from("Do trunc".as_bytes())
+        )
+    }
+
+    #[test]
+    fn dont_truncate() {
+        assert_eq!(
+            utf8_truncate(255, "Don't truncate, ASCII".as_bytes()).unwrap(),
+            Vec::from("Don't truncate, ASCII".as_bytes())
+        )
+    }
 }
