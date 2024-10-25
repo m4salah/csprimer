40ce3ac3a40c9adc1f5cfed059a56f80beda5fd0
Mohamed A. Salah <me@msalah.net>
2024-10-25 18:10:33 +0300

    make fibonacci

--- advanced_programming/trampoline/src/main.rs
+++ advanced_programming/trampoline/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 enum Trampoline<T> {
     Continue(Box<dyn FnOnce() -> Self>),
     Done(T),
@@ -30,7 +31,28 @@ fn factorial_tail(n: u64, acc: u64) -> u64 {
     return factorial_tail(n - 1, n * acc);
 }
 
+fn fib_trampoline(n: u64, a: u64, b: u64) -> Trampoline<u64> {
+    if n == 0 {
+        return Trampoline::Done(a);
+    }
+
+    return Trampoline::Continue(Box::new(move || {
+        return fib_trampoline(n - 1, b, a + b);
+    }));
+}
+
+fn fib_tail(n: u64, a: u64, b: u64) -> u64 {
+    if n == 0 {
+        return a;
+    }
+
+    return fib_tail(n - 1, b, a + b);
+}
+
 fn main() {
     println!("{}", run_trampoline(factorial_trampoline(20, 1)));
     println!("{}", factorial_tail(20, 1));
+
+    println!("{}", run_trampoline(fib_trampoline(20, 0, 1)));
+    println!("{}", fib_tail(20, 0, 1));
 }
