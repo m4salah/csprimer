d404cd58b63c865c47bc7d7984f51b8b1a1ba222
Mohamed A. Salah <me@msalah.net>
2024-10-25 17:58:51 +0300

    trampoline optmization and tail call to the factorial

--- advanced_programming/trampoline/src/main.rs
+++ advanced_programming/trampoline/src/main.rs
@@ -1,3 +1,36 @@
+enum Trampoline<T> {
+    Continue(Box<dyn FnOnce() -> Self>),
+    Done(T),
+}
+
+fn run_trampoline<T>(mut t: Trampoline<T>) -> T {
+    loop {
+        match t {
+            Trampoline::Continue(continue_fn) => t = continue_fn(),
+            Trampoline::Done(result) => return result,
+        }
+    }
+}
+
+fn factorial_trampoline(n: u64, acc: u64) -> Trampoline<u64> {
+    if n == 0 {
+        return Trampoline::Done(acc);
+    }
+
+    return Trampoline::Continue(Box::new(move || {
+        return factorial_trampoline(n - 1, acc * n);
+    }));
+}
+
+fn factorial_tail(n: u64, acc: u64) -> u64 {
+    if n == 0 {
+        return acc;
+    }
+
+    return factorial_tail(n - 1, n * acc);
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", run_trampoline(factorial_trampoline(20, 1)));
+    println!("{}", factorial_tail(20, 1));
 }
