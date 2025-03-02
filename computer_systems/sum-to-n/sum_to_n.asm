28fde302442f789740e73208214aa752f7ef2d29
Mohamed A. Salah <me@msalah.net>
2025-03-02 02:54:54 +0200

    optmize to remove one instruction
    it turns out that sub is modifying the tha flag which jmp instruction is looking

--- computer_systems/sum-to-n/sum_to_n.asm
+++ computer_systems/sum-to-n/sum_to_n.asm
@@ -7,13 +7,9 @@ sum_to_n:
 	; rax is the return value in unix x86 calling convention
 	xor rax, rax
 ; lable the loop
-sum_loop:
+_sum_loop:
 	; rdi is the first argument in unix x86 calling convention 
-	cmp rdi, 0   ; compare it zero
-	je done      ; if it's equal to zero then we are done
 	add rax, rdi ; add n to rax (the return value)
 	sub rdi, 1   ; subtract 1 from n (rdi)
-	jmp sum_loop ; loop again until we make rdi 0
-
-done:
+	jg _sum_loop ; loop again until we make rdi 0
 	ret
