12ece563504310008e9a2244a613683ee8a05ff2
Mohamed A. Salah <me@msalah.net>
2025-03-02 02:36:13 +0200

    solve the sum_to_n assembly

---
+++ computer_systems/sum-to-n/sum_to_n.asm
@@ -0,0 +1,19 @@
+global sum_to_n
+
+section .text
+
+sum_to_n:
+	; zero the return value
+	; rax is the return value in unix x86 calling convention
+	mov rax, 0
+; lable the loop
+sum_loop:
+	; rdi is the first argument in unix x86 calling convention 
+	cmp rdi, 0 ; compare it zero
+	je done    ; if it's equal to zero then we are done
+	add rax, rdi ; add n to rax (the return value)
+	sub rdi, 1   ; subtract 1 from rdi
+	jmp sum_loop ; loop again until we make rdi 0
+
+done:
+	ret
