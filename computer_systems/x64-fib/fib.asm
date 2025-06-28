88d13d3a05ba8400a43b79db22d0f884eb4ffb4e
Mohamed A. Salah <me@msalah.net>
2025-06-28 20:35:39 +0300

    solve fib in recursion

---
+++ computer_systems/x64-fib/fib.asm
@@ -0,0 +1,28 @@
+section .text
+global fib
+fib:
+	; rdi: is the input number n
+
+	; this is the base case
+	cmp rdi, 0
+	je fib_0
+	cmp rdi, 1
+	je fib_1
+
+	push rdi   ; save the original rdi
+	sub rdi, 1 ; subtract 1 to caluclate fib(rdi - 1)
+	call fib   ; fib(rdi - 1)
+	pop rdi    ; retrieve the original rdi
+	push rax   ; save fib(rdi - 1)
+	sub rdi, 2 ; rdi - 2
+	call fib   ; calculate fib(rdi - 2)
+	pop rbx    ; retrieve fib(rdi - 1) -> rbx
+	add rax, rbx ; rax now hase fib(rdi - 2), add to it fib(rdi - 1)
+	ret
+
+fib_0:
+	mov rax, 0
+	ret
+fib_1:
+	mov rax, 1
+	ret
